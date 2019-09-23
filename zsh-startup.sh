420ecc0a742deca2fab5b4d537ff0d5089466e0d
Timmy Douglas <timmy@timmyd.info>
2019-09-23 15:19:57 -0700

    add krmevicted

--- zsh-startup.sh
+++ zsh-startup.sh
@@ -143,6 +143,7 @@ alias ll='ls -l'
 alias k=kubectl
 alias ksn='kubectl get nodes | sed 1d | fzf +m | awk '\''{print $1}'\'''
 alias ksp='kubectl get pods | sed 1d | fzf +m | awk '\''{print $1}'\'''
+alias krmevicted='kubectl get po | grep Evicted| awk '\''{print $1}'\''| xargs -n 1 kubectl delete pod'
 
 git config --global include.path $HOME/config/gitalias.txt
 
