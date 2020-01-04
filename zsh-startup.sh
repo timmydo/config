50177e6193c11e767285f379ed2ddd99f4377571
Timmy Douglas <mail@timmydouglas.com>
2020-01-04 15:29:30 -0800

    aerc bind

--- zsh-startup.sh
+++ zsh-startup.sh
@@ -145,7 +145,7 @@ alias k=kubectl
 alias ksn='kubectl get nodes | sed 1d | fzf +m | awk '\''{print $1}'\'''
 alias ksp='kubectl get pods | sed 1d | fzf +m | awk '\''{print $1}'\'''
 alias krmevicted='kubectl get po | grep Evicted| awk '\''{print $1}'\''| xargs -n 1 kubectl delete pod'
-
+alias www='swaymsg exec -- epiphany --new-window'
 git config --global include.path $HOME/.config/gitalias.txt
 
 fd() {
