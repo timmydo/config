3810c3ce22cacbca8ae0e67b33ea055aa0c66122
Timmy Douglas <mail@timmydouglas.com>
2020-01-18 15:46:55 -0800

    fix

---
+++ start-offlineimap.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+systemctl start --user offlineimap@timmydouglascom.service
+systemctl start --user offlineimap@timmydinfo.service     
+systemctl start --user offlineimap@gmail.service     
