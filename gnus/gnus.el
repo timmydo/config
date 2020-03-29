d4ce3c0e160c804137de7d8cc48ccb569137a2bf
Timmy Douglas <mail@timmydouglas.com>
2020-03-29 16:45:01 -0700

    gnus

---
+++ gnus/gnus.el
@@ -0,0 +1,6 @@
+(setq gnus-select-method '(nnmaildir "td" (directory "~/mail/timmydouglascom")))
+(setq gnus-secondary-select-methods
+      '((nnmaildir "info" (directory "~/mail/timmydinfo"))
+	(nnmaildir "gmail" (directory "~/mail/gmail"))))
+	
+
