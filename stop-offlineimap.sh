579c71bd062dc55b2e0d7cd628272262535cfbfb
Timmy Douglas <mail@timmydouglas.com>
2020-05-03 09:03:58 -0700

    cleanupinit.el

---
+++ stop-offlineimap.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+systemctl stop --user offlineimap-oneshot@timmydouglascom.timer
+systemctl stop --user offlineimap-oneshot@timmydinfo.timer     
+systemctl stop --user offlineimap-oneshot@gmail.timer     
