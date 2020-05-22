73466f685e95576b2173e0d32600d480c6bbecd6
Timmy Douglas <mail@timmydouglas.com>
2020-05-22 10:22:33 -0700

    reorg-photo

--- reorg-photo.sh
+++ reorg-photo.sh
@@ -10,14 +10,15 @@ for file in "$@";do
     DNAME=$(dirname "$file")
     DATETIME=unknown
 
-    #if [[ "$DNAME" =~ .*([0-9][0-9][0-9][0-9])-([0-9][0-9])-[0-9][0-9].* ]]; then
-    #   YEAR=${BASH_REMATCH[1]}
-    #   MONTH=${BASH_REMATCH[2]}
-    #fi
+    DATETIME=$(exiftool -DateTimeOriginal -d %Y-%m-%d -j "$file" 2>/dev/null| jq -r '.[0].DateTimeOriginal')
+    if [[ "$DATETIME" =~ .*([0-9][0-9][0-9][0-9])-([0-9][0-9])-[0-9][0-9].* ]]; then
+	YEAR=${BASH_REMATCH[1]}
+	MONTH=${BASH_REMATCH[2]}
+    fi
+
 
     if [[ $YEAR == "unknown" ]]; then
-	DATETIME=$(exiftool -DateTimeOriginal -d %Y-%m-%d -j "$file" 2>/dev/null| jq -r '.[0].DateTimeOriginal')
-	if [[ "$DATETIME" =~ .*([0-9][0-9][0-9][0-9])-([0-9][0-9])-[0-9][0-9].* ]]; then
+	if [[ "$DNAME" =~ .*([0-9][0-9][0-9][0-9])-([0-9][0-9])-[0-9][0-9].* ]]; then
 	    YEAR=${BASH_REMATCH[1]}
 	    MONTH=${BASH_REMATCH[2]}
 	fi
