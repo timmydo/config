b72cb0dcc4aa40e058f53682f6e1f3eec9b3e917
Timmy Douglas <mail@timmydouglas.com>
2021-12-23 20:12:06 -0800

    updates

---
+++ create-links.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -ex
+mkdir -p ~/bin
+ln -svf ~/.config/start-sway.sh ~/bin/start-sway
+ln -svf ~/.config/cosa/run.sh ~/bin/cosa
+ln -svf ~/.config/start-sway.sh ~/bin/start-sway
+echo "source $HOME/.config/zsh-startup.sh" > ~/.zshrc
+
+
