f2e9f4ab19886fc8ae443d291d165c35283fab09
Timmy Douglas <mail@timmydouglas.com>
2021-12-30 16:02:07 -0800

    svscan

--- create-links.sh
+++ create-links.sh
@@ -5,6 +5,7 @@ mkdir -p ~/bin
 ln -svf ~/.config/start-sway.sh ~/bin/start-sway
 ln -svf ~/.config/cosa/run.sh ~/bin/cosa
 ln -svf ~/.config/start-sway.sh ~/bin/start-sway
+ln -svf ~/.config/svscan.sh ~/bin/svscan
 echo "source $HOME/.config/zsh-startup.sh" > ~/.zshrc
 
 
